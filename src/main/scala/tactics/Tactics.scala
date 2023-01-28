90f4dac34911f49ee002a33f0a6d814c6063ca8e
Martin Berger <contact@martinfriedrichberger.net>
2023-01-28 23:17:11 +0000

    Reverted to old formatting

--- src/main/scala/tactics/Tactics.scala
+++ src/main/scala/tactics/Tactics.scala
@@ -10,16 +10,16 @@ case class OrElse(fst: Tactic, snd: Tactic)   extends Tactic
 case class Repeat(tac: Tactic)                extends Tactic
 case class Select(subgoals: List[String])     extends Tactic
 case class PrintState(active: Boolean = true) extends Tactic
-case class SelectLast() extends Tactic // TODO Experimental, if it works can be unified with Select(l) above
+case class SelectLast()                       extends Tactic // TODO Experimental, if it works can be unified with Select(l) above
 
 object Tactic:
-   def AndThenList(l: List[Tactic]): Tactic =
-      println(s"[AndThenList] found ${l.size}")
-      l match
-         case List()      => Id()
-         case fst :: rest => AndThen(fst, AndThenList(rest))
+    def AndThenList(l: List[Tactic]): Tactic =
+        // println(s"[AndThenList] found ${l.size}") TODO remove junk
+        l match
+            case List()      => Id()
+            case fst :: rest => AndThen(fst, AndThenList(rest))
 
-   def makeGeneric(l: List[PreTactic], printState: Boolean = true): List[Tactic] =
-      val l1 = l.map(Apply(_))
-      val l2 = Lib.sandwich(SelectLast())(l1)
-      if printState then Lib.saturate(PrintState(true))(l2) else l2
+    def makeGeneric(l: List[PreTactic], printState: Boolean = false): List[Tactic] =
+        val l1 = l.map(Apply(_))
+        val l2 = Lib.sandwich(SelectLast())(l1)
+        if printState then Lib.saturate(PrintState(true))(l2) else l2
