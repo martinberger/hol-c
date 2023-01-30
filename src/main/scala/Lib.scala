1e8501b4f7dea5ff5f3ee8436e0a816a99830885
Martin Berger <contact@martinfriedrichberger.net>
2023-01-30 01:32:59 +0000

    Cleaned up Scala-2 code

--- src/main/scala/Lib.scala
+++ src/main/scala/Lib.scala
@@ -28,34 +28,27 @@ object Lib:
                     case None         => None
                     case Some(result) => Some(t :: result)
 
-    def sandwichMerge[A](empty: A, add: (A, A) => A)(between: A)(l: Seq[A]): A = {
+    def sandwichMerge[A](empty: A, add: (A, A) => A)(between: A)(l: List[A]): A =
         import scala.annotation.tailrec
-        @tailrec def aux(accu: A)(l: Seq[A]): A = {
-            l match { // WARNING, the below is false, since it only matches on LISTS what if this is not a list?
-                case Seq()          => accu
-                case Seq(a)         => add(accu, a)
+        @tailrec def aux(accu: A)(l: List[A]): A =
+            l match
+                case List()         => accu
+                case List(a)        => add(accu, a)
                 case a :: b :: rest => aux(add(add(accu, a), between))(b :: rest)
-            }
-        }
         (aux(empty)(l))
-    }
 
-    def sandwichMerge(between: String)(l: Seq[String]): String = {
+    def sandwichMerge(between: String)(l: List[String]): String =
         def add(s1: String, s2: String): String = { s"${s1}${s2}" }
         sandwichMerge("", add _)(between)(l)
-    }
 
-    def sandwich[A](between: A)(l: List[A]): List[A] = {
+    def sandwich[A](between: A)(l: List[A]): List[A] =
         import scala.annotation.tailrec
-        @tailrec def aux(accu: List[A])(l: List[A]): List[A] = {
-            l match { // WARNING, the below is false, since it only matches on LISTS what if this is not a list?
+        @tailrec def aux(accu: List[A])(l: List[A]): List[A] =
+            l match
                 case List()         => accu
                 case List(_)        => accu ++ l
                 case a :: b :: rest => aux(accu ++ List(a, between))(b :: rest)
-            }
-        }
         (aux(List())(l))
-    }
 
     def saturate[A](between: A)(l: List[A]): List[A] =
         between :: (sandwich(between)(l)) ++ List(between)
