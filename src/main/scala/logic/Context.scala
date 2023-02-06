0d671f00ad70154992ed96dbd4506058af9053c4
Martin Berger <contact@martinfriedrichberger.net>
2023-02-06 10:50:53 +0000

    Cleaned up code that wasn't fully refactored.

--- src/main/scala/logic/Context.scala
+++ src/main/scala/logic/Context.scala
@@ -5,7 +5,7 @@ object Context:
     type Context = List[Term]
 
     def valid(gamma: Context): Boolean             = gamma.forall(tm => { Term.check(tm, Prop()) })
-    def fv(gamma: Context): Set[Var]               = gamma.toList.foldLeft(Set.empty)((accu, tm) => accu ++ Term.fv(tm))
-    def tySubst(gamma: Context, ty: Ty, tv: TyVar) = gamma.toList.map(Term.tySubst(_, ty, tv))
-    def subst(gamma: Context, tm: Term, x: Var)    = gamma.toList.map(Term.subst(_, tm, x))
+    def fv(gamma: Context): Set[Var]               = gamma.foldLeft(Set.empty)((accu, tm) => accu ++ Term.fv(tm))
+    def tySubst(gamma: Context, ty: Ty, tv: TyVar) = gamma.map(Term.tySubst(_, ty, tv))
+    def subst(gamma: Context, tm: Term, x: Var)    = gamma.map(Term.subst(_, tm, x))
     def remove(gamma: Context, tm: Term): Context  = gamma.diff(List(tm))
