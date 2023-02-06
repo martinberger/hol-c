ebb00b8e9c9fd2a4d0f910d0587aab2c13ad3f16
Martin Berger <contact@martinfriedrichberger.net>
2023-02-06 14:24:13 +0000

    Simplified code

--- src/main/scala/logic/Context.scala
+++ src/main/scala/logic/Context.scala
@@ -4,7 +4,7 @@ object Context:
 
     type Context = List[Term]
 
-    def valid(gamma: Context): Boolean             = gamma.forall(tm => { Term.check(tm, Prop()) })
+    def valid(gamma: Context): Boolean             = gamma.forall(Term.check(_, Prop()))
     def fv(gamma: Context): Set[Var]               = gamma.foldLeft(Set.empty)((accu, tm) => accu ++ Term.fv(tm))
     def tySubst(gamma: Context, ty: Ty, tv: TyVar) = gamma.map(Term.tySubst(_, ty, tv))
     def subst(gamma: Context, tm: Term, x: Var)    = gamma.map(Term.subst(_, tm, x))
