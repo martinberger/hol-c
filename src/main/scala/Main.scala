5dcc63e33524c05944778e5a20dd404372c11c15
Martin Berger <contact@martinfriedrichberger.net>
2023-02-09 12:16:51 +0000

    Refactored testing setup for clarity

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,22 +1,5 @@
 object Main:
-
-    type Runner = (() => (Int, Int))
-    def run(tests: List[(Runner, String)]): Int =
-        def eval(runner: Runner, name: String): Int =
-            println(s"\n----------- ${name} Tests -----------")
-            val (all, failed) = runner()
-            println(s"Passed ${all - failed} out of ${all} tests")
-            failed
-        tests.foldLeft(0)((accu, t) => accu + eval(t._1, t._2))
-
-    import Prover._
-    val allTests = List(
-      (KindTests.run _, "Kind"),
-      (TypeTests.run _, "Type"),
-      (TermTests.run _, "Term"),
-      (RuleTests.run _, "Rule"),
-      (TacTests.run _, "Tactic")
-    )
+    import Prover.Tests.{run, allTests}
 
     def main(argv: Array[String]): Unit =
         val failed = run(allTests)
