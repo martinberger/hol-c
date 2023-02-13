c5ed2535aee527fc435f928253b05d9aa2779e9f
Martin Berger <contact@martinfriedrichberger.net>
2023-02-13 15:05:14 +0000

    Added explanation

--- src/main/scala/logic/Taint.scala
+++ src/main/scala/logic/Taint.scala
@@ -19,8 +19,8 @@ given CanEqual[Taint, Taint] = CanEqual.derived
 //    I
 //
 //  Note that taint in general is not a linear order.  The 4 taints we
-//  use in the paper accompanying this implementation just happens to
-//  be linear.
+//  use in the paper accompanying this implementation just happen to
+//  be mutually comparable, hence the resulting order is linear.
 
 object TaintLattice extends JoinSemilattice:
     type T = Taint
